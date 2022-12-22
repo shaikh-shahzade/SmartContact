c11a620dd89059b44ff857f29ef360b715fb0cbd
shaikh shahzade <shahzade10@gmail.com>
2022-12-22 14:31:16 +0530

    first commit

---
+++ src/test/java/com/smart/SmartContractTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContractTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
